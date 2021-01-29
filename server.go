40e6077286ad90aa93adba0e91abe0cf0628168c
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2021-01-29 18:55:53 -0300

    golang: GET and JSON Parsing added

--- server.go
+++ server.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/urfave/negroni"
 )
 
+type Data []struct {
+	BRL string `json:""`
+}
+
 func main() {
 	mux := http.NewServeMux()
+
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Welcome to the home page!")
+
+		res, err := http.Get("https://api.exchangeratesapi.io/latest?base=BRL&symbols=BRL,USD")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		body, readErr := ioutil.ReadAll(res.Body)
+
+		if readErr != nil {
+			log.Fatal(readErr)
+		}
+
+		fmt.Println(string(body))
 	})
 
 	n := negroni.Classic() // Includes some default middlewares
