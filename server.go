86e9345703f488cc186df1818b5b0b3dc3499509
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2021-01-30 00:34:31 -0300

    golang: Finalization & addition of comments

--- server.go
+++ server.go
@@ -10,39 +10,33 @@ import (
 	"github.com/urfave/negroni"
 )
 
-type Data struct {
-	Rates *Rates `json:"rates"`
-}
-
-type Rates struct {
-	BRL string `json:"brl"`
-	USD string `json:"usd"`
-	EUR string `json:"eur"`
-}
-
 func main() {
+	// Mux start
 	mux := http.NewServeMux()
 
+	// Argument parameter handling
 	param := ""
-
-	param = os.Args[1]
-
 	if len(os.Args) > 1 {
 		param = os.Args[1]
 	} else {
 		param = "BRL"
 	}
 
+	// Default route declaration
 	mux.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
 
+		// Exchange Rates API link that will be consumed
 		res, err := http.Get("https://api.exchangeratesapi.io/latest?base=" + param + "&symbols=BRL,USD,EUR")
 
+		// Error Handling
 		if err != nil {
 			log.Fatal(err)
 		}
 
+		// JSON data handling
 		body, readErr := ioutil.ReadAll(res.Body)
 
+		// JSON error handling
 		if readErr != nil {
 			log.Fatal(readErr)
 		}
@@ -50,8 +44,10 @@ func main() {
 		fmt.Fprintf(w, string(body))
 	})
 
+	// Negroni default declarations
 	n := negroni.Classic() // Includes some default middlewares
 	n.UseHandler(mux)
 
+	// API port that will be used
 	http.ListenAndServe(":3000", n)
 }
