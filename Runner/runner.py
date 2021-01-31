2aa8d3ec295095b59ee229157fb55529c2ed9a5e
GabrielCastroSilva <gabriel.silva.010@acad.pucrs.br>
2021-01-30 23:46:47 -0300

    docker: docker implementation

--- Runner/runner.py
+++ Runner/runner.py
@@ -1,20 +1,30 @@
+################################################
+#                                              #
+#          Simple Python Runner                #
+#                                              #
+################################################
+
+#Initial imports
 import urllib.request
 import json
 import iso4217parse
 
-link = "http://localhost:3000/"
+# Declaring API url
+link = "http://host.docker.internal:3000/"
+
+# Fetching data from API through request
 f = urllib.request.urlopen(link)
 myfile = f.read()
 
+# Parsing data from API's JSON
 rates = json.loads(myfile)
-
 rawData = iso4217parse.parse(str(rates['base']))
 
-print(rawData[0][3][0])
-
+# Declaring important data for usage in prints
 coin = rawData[0][2]
 symbol = rawData[0][3][0]
 
+# Prints showcasing parsed data from API
 print("Moeda escolhida: " + coin)
 print(symbol + " 1,00 se equivale a:")
 print("€ " + str(rates['rates']['EUR']) + " Euros")
